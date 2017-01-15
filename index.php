65acd4e33a09b11ad1da9243836d7a149b28b2ab
MikaelFeher <mikael.feher@gmail.com>
2017-01-15 21:38:02 +0100

    Adds php form validation

--- index.php
+++ index.php
@@ -9,23 +9,48 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
 	<style rel="stylesheet" type="text/css" media="screen">
+
 		#successMessage {
 			display: none;
 		}
 		#erMsg {
 			display: none;
 		}
+		.error {
+			color: red;
+		}
+
 	</style>
   </head>
   <body>
 	<?php
 		// Form validation using PHP.
+		$emailErr = $subjectErr = $messageErr = '';
+		$email = $subject = $message = '';
 
 		if($_SERVER["REQUEST_METHOD"] == "POST"){
-			$email = test_input($_POST['email']);
-			$subject = test_input($_POST['subject']);
-			$message = test_input($_post['messageBody']);
-			echo "SUCCESS!!!!";
+
+			if (empty($_POST['email'])) {
+				$emailErr = 'A valid email address is required';
+			} else {
+				$email = test_input($_POST['email']);
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+					$emailErr = "Invalid email format";
+				}
+			}
+
+			if (empty($_POST['subject'])) {
+				$subjectErr = "Subject line can't be empty";
+			} else {
+				$subject = test_input($_POST['subject']);
+			}
+
+			if (empty($_POST['messageBody'])) {
+				$messageErr = "Please write a message. We aren't mindreaders :)";
+			} else {
+				$message = test_input($_post['messageBody']);
+			}
+
 		}
 
 		function test_input($data) {
@@ -34,13 +59,6 @@
 			$data = htmlspecialchars($data);
 			return $data;
 		}
-
-
-
-		if ($_POST) {
-			print_r($_POST);
-		}
-
 
 	 ?>
 	<div class="container">
@@ -54,29 +72,25 @@
 
 		</div>
 
+		<p class="error">* required field.</p>
+
 		<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 			<div class="form-group">
-				<label for="email">Email address</label>
-				<input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-				<small id="emailHelp" class="form-text text-muted"></small>
+				<label for="email">Email address <span class="error">* <?php echo $emailErr; ?></label>
+				<input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email" value='<?php echo $email; ?>'></span><small id="emailHelp" class="form-text text-muted"></small>
 			</div>
 			<div class="form-group">
-				<label for="subject">Subject</label>
-				<input type="text" class="form-control" name="subject" id="subject" aria-describedby="subject">
+				<label for="subject">Subject <span class="error">* <?php echo $subjectErr; ?></span></label>
+				<input type="text" class="form-control" name="subject" id="subject" aria-describedby="subject" value='<?php echo $subject; ?>'>
 			</div>
 			<div class="form-group">
-				<label for="messageBody">What would you like to ask us?</label>
-				<textarea class="form-control" name="messageBody" id="messageBody" rows="5"></textarea>
+				<label for="messageBody">What would you like to ask us? <span class="error">* <?php echo $messageErr; ?></span></label>
+				<textarea class="form-control" name="messageBody" id="messageBody" rows="5"><?php echo $message; ?></textarea>
 			</div>
 			<button type="submit" class="btn btn-primary" id="submitButton">Submit</button>
 
 		</form>
 	</div>
-
-
-
-
-
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
@@ -88,7 +102,6 @@
 			var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 			return regex.test(email);
 		}
-
 
 		$('#submitButton').click(function () {
 			var errorMessage = '';
@@ -122,6 +135,7 @@
 			} else {
 				$('#successMessage').show();
 				$('#erMsg').hide();
+				$.post('index.php');
 			}
 
 			return false;
